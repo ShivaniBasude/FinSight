c24701a2556d529d579c0ec6bb9241566660243e
ShivaniBasude <shivanibasude@gmail.com>
2026-04-05 23:44:49 +0530

    Changes

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import "./style.css";
 const rootElement = document.getElementById("root");
 if (rootElement) {
   ReactDom.createRoot(rootElement).render(
-    <BrowserRouter basename="/finsight">
+    <BrowserRouter>
       <AppProvider>
         <App />
       </AppProvider>
